f29451b0b5a45506924d21672c955789dd1dfe96
TonyAng <13323650885@163.com>
2017-02-24 16:51:40 +0800

    Initial Commit

---
+++ TonyAngRightSelectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TonyAngRightSelectDemo
+//
+//  Created by TonyAng on 2017/2/24.
+//  Copyright © 2017年 TonyAng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
